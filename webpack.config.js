ac82eb6059ffd07dc4f34b4b68c99afd1194d605
Sergey <sergeylabetsik@datateer.com>
2023-07-06 18:30:59 -0500

    Establish environment

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  mode: "production",
+  devtool: "inline-source-map",
+  entry: {
+    main: "./src/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "[name]-bundle.js", // <--- Will be compiled to this single file
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  optimization: {
+    minimize: true,
+  },
+};
